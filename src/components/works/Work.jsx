ffd5bcc72b15ea7a38772423378a9dab13e3f4db
majdalsaeed <majd.alsaeed13m@gmail.com>
2021-11-10 12:48:06 +0100

    added images to works page

--- src/components/works/Work.jsx
+++ src/components/works/Work.jsx
@@ -1,18 +1,26 @@
 
-import "./work.scss"
-
+import "./work.scss";
+import img2 from "../image/BikeTime.png";
+import img3 from "../image/DatingApp.png";
 export default function Work() {
     return (
         <div className="works" id="works">
             <div className="slider">
                 <div className="container">
-                    <div className="item">
                         <div className="left">
-                            <div className="leftContainer"></div>
-                            <div className="iconContainer"></div>
+                            <div className="item">
+                                <img src={img2} alt="" className="img2" />
+                                <h3>BikeTime</h3>
+                            </div>
+                            <div className="item">
+                            <img src={img3} alt="" className="img2" />
+                                <h3>Dating App</h3>
+                            </div>
+                            
                         </div>
-                        <div className="right"></div>
-                    </div>
+                        <div className="right">
+                        
+                        </div>
                 </div>
             </div>
         </div>
